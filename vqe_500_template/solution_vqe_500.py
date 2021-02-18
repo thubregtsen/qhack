0850256234d449cbde04dcbe899657401bc70c5e
David Wierichs <davidwierichs@gmail.com>
2021-02-18 01:21:34 +0100

    davids attempt at vqe_500

--- vqe_500_template/solution_vqe_500.py
+++ vqe_500_template/solution_vqe_500.py
@@ -4,6 +4,19 @@ import sys
 import pennylane as qml
 import numpy as np
 
+def ansatz(params, wires):
+    N = len(wires)
+    n = len(params)
+    sub_n = 3*N
+    L = n//sub_n
+    for l in range(L):
+        for i in range(N):
+            qml.RZ(params[l*sub_n+i], wires=wires[i])
+            qml.RX(params[l*sub_n+i+1], wires=wires[i])
+            qml.RZ(params[l*sub_n+i+2], wires=wires[i])
+        for i in range(N):
+            qml.CNOT(wires=[wires[i], wires[(i+1)%N]])
+
 
 def find_excited_states(H):
     """
@@ -22,10 +35,28 @@ def find_excited_states(H):
     energies = np.zeros(3)
 
     # QHACK #
+    N = len(H.wires)
+    L = N
+    params = np.random.random(L*3*N)*0.5
+    dev = qml.device('default.qubit', wires=N)
+    cost = qml.ExpvalCost(ansatz, H, dev)
+    opt = qml.AdamOptimizer(stepsize=0.30, beta1=0.9, beta2=0.999, eps=1e-7)
+
+    energy = 0.
+    for i in range(500):
+        last_cost = energy
+        if i%10==0:
+            print(f"At step {i}, have cost {energy} at params {params}.")
+            print(qnode(params))
+        params, energy = opt.step_and_cost(cost_fn, params)
+        if np.abs(energy - last_cost)<5e-4 and np.linalg.norm(qml.grad(cost_fn)(params))<1e-2:
+            solved = True
+            break
 
     # QHACK #
 
-    return ",".join([str(E) for E in energies])
+    return energy
+    #return ",".join([str(E) for E in energies])
 
 
 def pauli_token_to_operator(token):
