c640651a2dc5479452c7fe722a901821322e0c7a
Tom <thubregtsen@gmail.com>
2021-02-23 20:41:44 +0000

    merge

--- wp_TK/Floq.py
+++ wp_TK/Floq.py
@@ -16,28 +16,10 @@
 f = open("floq_key", "r")
 floq_key = f.read().replace("\n", "")
 
-# +
-from remote_cirq import RemoteSimulator
-import cirq
-import sympy
-
-sim = RemoteSimulator(floq_key)
-
-qubits = cirq.LineQubit.range(26)
-
-param_resolver = cirq.ParamResolver({'a': 1})
-
-a = sympy.Symbol('a')
-circuit = cirq.Circuit(
-        [cirq.X(q) ** a for q in qubits] +
-        [cirq.measure(q) for q in qubits])
-
-sim.run(circuit, param_resolver) #Results from the cloud hurray!
-
-# +
 import remote_cirq
 import pennylane as qml
 import numpy as np
+import time
 import sys
 wires = 26
 np.random.seed(0)
@@ -46,15 +28,22 @@ weights = np.random.randn(1, wires, 3)
 API_KEY = floq_key
 sim = remote_cirq.RemoteSimulator(API_KEY)
 
-dev = qml.device("cirq.simulator",
-                 wires=wires,
-                 simulator=sim,
-                 analytic=False)
-
+#dev = qml.device("cirq.simulator",
+#                 wires=wires,
+#                 simulator=sim,
+#                 analytic=False)
+dev = qml.device("default.qubit", wires=wires)
 @qml.qnode(dev)
 def my_circuit(weights):
         qml.templates.layers.StronglyEntanglingLayers(weights,
                                                       wires=range(wires))
         return qml.expval(qml.PauliZ(0))
 
-print(my_circuit(weights))
+start_t = time.time()
+start_e = dev.num_executions
+my_circuit(weights)
+stop_t = time.time()
+stop_e = dev.num_executions
+print("time", stop_t - start_t)
+print("execution", stop_e - start_e)
+
