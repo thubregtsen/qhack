66b319834d95b34922320e9d65ccb95e3fde33e2
thubregtsen <thubregtsen@gmail.com>
2021-02-24 21:24:13 +0100

    plots

--- plots_and_data/Plot_limited.py
+++ plots_and_data/Plot_limited.py
@@ -81,7 +81,7 @@ for line in data:
     qvalidate_e.append(float(words[13]))
 # -
 
-qembKer_t
+len(embKer_t)
 
 import matplotlib.pyplot as plt
 
@@ -101,12 +101,13 @@ plt.figure(figsize=(10,5))
 plt.title("Increase in qubits with len(X) = " + str(n_X) + " and epoch = 1")
 plt.ylabel("Number of circuit executions")
 plt.xlabel("number of qubits")
+class_stop = len(embKer_t) + 2
 range_start = 26
 range_stop = 26+len(qembKer_t)
-plt.plot(range(2,26), embKer_e, label="classical kern.EmbeddingKernel()")
-plt.plot(range(2,26), opt_kernel_e, label="classical optimize_kernel_param()")
-plt.plot(range(2,26), train_svm_e, label="classical train_svm()")
-plt.plot(range(2,26), validate_e, label="classical validate()")
+plt.plot(range(2,class_stop), embKer_e, label="classical kern.EmbeddingKernel()")
+plt.plot(range(2,class_stop), opt_kernel_e, label="classical optimize_kernel_param()")
+plt.plot(range(2,class_stop), train_svm_e, label="classical train_svm()")
+plt.plot(range(2,class_stop), validate_e, label="classical validate()")
 plt.plot(range(range_start,range_stop), qembKer_e, label="qpu kern.EmbeddingKernel()")
 plt.plot(range(range_start,range_stop), qopt_kernel_e, label="qpu optimize_kernel_param()")
 plt.plot(range(range_start,range_stop), qtrain_svm_e, label="qpu train_svm()")
@@ -116,7 +117,7 @@ plt.legend()
 plt.show()
 # -
 
-len(qembKer_t)
+validate_t
 
 [validate_t[i]/validate_e[i] for i in range(len(validate_t))]
 
@@ -136,10 +137,10 @@ plt.figure(figsize=(10,5))
 plt.title("Increase in qubits with len(X) = " + str(n_X) + " and epoch = 1")
 plt.ylabel("time for full function to complete (seconds)")
 plt.xlabel("number of qubits")
-plt.plot(range(2,26), embKer_t, label="classical kern.EmbeddingKernel()")
-plt.plot(range(2,26), opt_kernel_t, label="classical optimize_kernel_param()")
-plt.plot(range(2,26), train_svm_t, label="classical train_svm()")
-plt.plot(range(2,26), validate_t, label="classical validate()")
+plt.plot(range(2,class_stop), embKer_t, label="classical kern.EmbeddingKernel()")
+plt.plot(range(2,class_stop), opt_kernel_t, label="classical optimize_kernel_param()")
+plt.plot(range(2,class_stop), train_svm_t, label="classical train_svm()")
+plt.plot(range(2,class_stop), validate_t, label="classical validate()")
 plt.plot(range(range_start,range_stop), qembKer_t, label="qpu kern.EmbeddingKernel()")
 plt.plot(range(range_start,range_stop), qopt_kernel_t, label="qpu optimize_kernel_param()")
 plt.plot(range(range_start,range_stop), qtrain_svm_t, label="qpu train_svm()")
@@ -147,6 +148,16 @@ plt.plot(range(range_start,range_stop), qvalidate_t, label="qpu validate()")
 #plt.yscale('log')
 plt.legend()
 plt.show()
+# -
+
+print(len(validate_e))
+
+import numpy as np
+np.asarray([validate_t[i]/validate_e[i] for i in range(len(validate_t))])
+
+np.asarray([qvalidate_t[i]/qvalidate_e[i] for i in range(len(qvalidate_t))])
+
+130/1.44569767
 
 # +
 #embKer_t = []
@@ -162,10 +173,10 @@ plt.figure(figsize=(10,5))
 plt.title("Increase in qubits with len(X) = " + str(n_X) + " and epoch = 1")
 plt.ylabel("Total function time / #function calls (seconds)")
 plt.xlabel("number of qubits")
-plt.plot(range(2,26), embKer_t, label="classical kern.EmbeddingKernel()")
-#plt.plot(range(2,26), [opt_kernel_t[i]/opt_kernel_e[i] for i in range(len(opt_kernel_t))], label="classical optimize_kernel_param()")
-plt.plot(range(2,26), [train_svm_t[i]/train_svm_e[i] for i in range(len(train_svm_t))], label="classical train_svm()")
-plt.plot(range(2,26), [validate_t[i]/validate_e[i] for i in range(len(validate_t))], label="classical validate()")
+plt.plot(range(2,class_stop), embKer_t, label="classical kern.EmbeddingKernel()")
+#plt.plot(range(2,class_stop), [opt_kernel_t[i]/opt_kernel_e[i] for i in range(len(opt_kernel_t))], label="classical optimize_kernel_param()")
+plt.plot(range(2,class_stop), [train_svm_t[i]/train_svm_e[i] for i in range(len(train_svm_t))], label="classical train_svm()")
+plt.plot(range(2,class_stop), [validate_t[i]/validate_e[i] for i in range(len(validate_t))], label="classical validate()")
 plt.plot(range(range_start,range_stop), qembKer_t, label="qpu kern.EmbeddingKernel()")
 #plt.plot(range(range_start,range_stop), [qopt_kernel_t[i]/qopt_kernel_e[i] for i in range(len(qopt_kernel_t))], label="qpu optimize_kernel_param()")
 plt.plot(range(range_start,range_stop), [qtrain_svm_t[i]/qtrain_svm_e[i] for i in range(len(qtrain_svm_t))], label="qpu train_svm()")
@@ -238,6 +249,9 @@ plt.legend()
 plt.show()
 # -
 len(sembKer_t)
+
+
+
 
 
 
