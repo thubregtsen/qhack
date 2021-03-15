6e531e5c8cc281256f6cb742fd5f22f88648d933
David Wierichs <davidwierichs@gmail.com>
2021-03-15 12:05:38 +0100

    restructured wp-NK data storage. finished wp_NK code for 2d sweep.

--- wp_NK/nk_lib.py
+++ wp_NK/nk_lib.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pennylane as qml
 from collections.abc import Iterable
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def closest_psd_matrix(K, solver=None, fix_diagonal=False):
     """Return the closest positive semidefinite matrix to the given kernel matrix.
@@ -142,20 +144,21 @@ def add_noise_channel(operation_list, noise_channel, idling_gate_noise_channel=N
 
     return all_ops
 
-def mitigate_global_depolarization(kernel_matrix, num_wires, strategy='average', use_entries=None):
+def mitigate_global_depolarization(kernel_matrix, num_wires, strategy='average', use_entries=None, return_noise_estimates=False):
     """Estimate the noise rate of a global depolarizing noise model based on the diagonal entries of a kernel
     matrix and mitigate the effect of said noise model.
     Args:
       kernel_matrix (ndarray): Noisy kernel matrix.
       num_wires (int): Number of wires/qubits that was used to compute the kernel matrix.
-      strategy ('average'|'split_channel'|None): Details of the noise model and strategy for mitigation.
+      strategy='average' ('average'|'split_channel'|None): Details of the noise model and strategy for mitigation.
         'average': Compute the noise rate based on the diagonal entries in use_entries, average if applicable.
         'split_channel': Assume a distinct effective noise rate for the embedding circuit of each feature vector.
         None: Don't do anything.
-      use_entries (list<int>): Indices of diagonal entries to use if strategy=='average'. Set to all if None.
+      use_entries=None (list<int>): Indices of diagonal entries to use if strategy=='average'. Set to all if None.
+      return_noise_estimates=False (bool): Whether or not to return the estimated noise rate(s).
     Returns:
       mitigated_matrix (ndarray): Mitigated kernel matrix.
-      noise_rates (ndarray): Determined noise rates, meaning depends on kwarg strategy.
+      noise_rates (ndarray): Determined noise rates, meaning depends on kwarg strategy, only returned if activated.
     Comments:
       If strategy is 'average', the diagonal entries with indices use_entries have to be measured on the QC.
       If it is 'split_channel', all diagonal entries are required.
@@ -185,5 +188,107 @@ def mitigate_global_depolarization(kernel_matrix, num_wires, strategy='average',
             + noise_rates.reshape((n, 1))
 #         print(inverse_noise)
         mitigated_matrix = (kernel_matrix - inverse_noise / dim) / (1 - inverse_noise)
+    if return_noise_estimates:
+        return mitigated_matrix, noise_rates
+    else:
+        return mitigated_matrix
 
-    return mitigated_matrix, noise_rates
+
+class noisy_kernel(qml.kernels.EmbeddingKernel):
+    """adds noise to a QEK
+    Args:
+      ansatz (callable): See qml.kernels.EmbeddingKernel
+      device (qml.Device, Sequence[Device]): See qml.kernels.EmbeddingKernel
+      noise_channel (callable): Noise channel to be applied on the level of noise_application_level.
+        Signature: (*args_noise_channel, operation_data, wires) -> list<qml.Operation>
+      args_noise_channel (dict): arguments to be passed to the noise_channel.
+      noise_application_level (str):
+        'global': Apply noise after the full circuit.
+        'per_embedding': Apply noise after the embedding and the inverted embedding each.
+        'per_gate': Apply noise after each gate (incompatible with noise_channels acting on all qubits).
+      idling_gate_noise_channel=None (callable|bool): Noise channel to be applied after an idling gate.
+        Signature: (wires) -> list<qml.Operation>
+        If True, use noise_channel with default data kwarg, if False, do not apply noise to idle qubits.
+    Comments:
+      WARNING: The noise_channel must be able to take any number of operation_data arguments that
+               occurs in the ansatz, e.g. 0 for unparametrized gates, 1 for simple rotations and so on.
+               When using noise_application_level in ('global', 'per_embedding') or 
+               idling_gate_noise_channel==True, a default must be set for the operation_data argument(s).
+    """
+
+    def __init__(
+        self,
+        ansatz,
+        device,
+        noise_channel,
+        args_noise_channel,
+        noise_application_level,
+        idling_gate_noise_channel=False,
+        **kwargs
+    ):
+        self.probs_qnode = None
+        self.noise_channel = noise_channel
+        self.args_noise_channel = args_noise_channel
+        self.noise_application_level = noise_application_level
+        # Make the noise_channel callable on operator data and wires but incorporate args_noise_channel
+        self.noise_channel_mapped = lambda data, wires: noise_channel(*args_noise_channel, data, wires=wires)
+        if idling_gate_noise_channel is True:
+            self.idling_gate_noise_channel = lambda wires: self.noise_channel(*args_noise_channel, wires=wires)
+        if idling_gate_noise_channel is False:
+            self.idling_gate_noise_channel = None
+        else:
+            self.idling_gate_noise_channel = idling_gate_noise_channel
+
+        def circuit(x1, x2, params, **kwargs):
+            if self.noise_application_level == 'per_gate':
+                add_noise_channel(
+                    ansatz(x1, params, **kwargs),
+                    self.noise_channel_mapped,
+                    self.idling_gate_noise_channel,
+                )
+            else:
+                ansatz(x1, params, **kwargs)
+
+            if self.noise_application_level == 'per_embedding':
+                # Invoke noise channel with default kwarg data.
+                self.noise_channel(*self.args_noise_channel, wires=device.wires)
+
+            elif self.noise_application_level == 'per_gate':
+                add_noise_channel(
+                    ansatz(x2, params, **kwargs),
+                    self.noise_channel_mapped,
+                    self.idling_gate_noise_channel,
+                    adjoint=True,
+                )
+            else:
+                qml.inv(ansatz(x2, params, **kwargs))
+
+            if self.noise_application_level in ('per_embedding', 'global'):
+                # Invoke noise channel with default kwarg data.
+                self.noise_channel(*self.args_noise_channel, wires=device.wires)
+
+            return qml.probs(wires=device.wires)
+
+        self.probs_qnode = qml.QNode(circuit, device, **kwargs)
+
+def visualize_kernel_matrices(kernel_matrices, draw_last_cbar=False):
+    num_mat = len(kernel_matrices)
+    width_ratios = [1]*num_mat+[0.2]*int(draw_last_cbar)
+    figsize = (num_mat*5+draw_last_cbar, 5)
+    num_axes = num_mat+draw_last_cbar
+    fig,ax = plt.subplots(1, num_axes, figsize=figsize, gridspec_kw={'width_ratios': width_ratios})
+
+    for i, kernel_matrix in enumerate(kernel_matrices):
+        plot = sns.heatmap(
+            kernel_matrix,
+            vmin=0,
+            vmax=1,
+            xticklabels='',
+            yticklabels='',
+            ax=ax[i],
+            cmap='Spectral',
+            cbar=False
+        )
+    if draw_last_cbar:
+        ch = plot.get_children()
+        fig.colorbar(ch[0], ax=ax[-2], cax=ax[-1])
